4979f68b78e26173034b9009c51cdf6c1c89f860
kalpa <kalpa@kakao.com>
2023-10-12 13:33:29 +0900

    add async and promise

--- src/welcome/AsyncPromises.jsx
+++ src/welcome/AsyncPromises.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
+// jsonplaceholder.typicode.com
 const AsyncPromises = () => {
+
+   const [todos, setTodos] = useState([])
+
+   // function fetchData() {
+   //    fetch('https://jsonplaceholder.typicode.com/todos')
+   //        .then(response => response.json())
+   //        .then(json => setTodos(json))
+   // }
+   async function getTodos() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+      const data = await res.json()
+      setTodos(data)
+      // console.log(data)
+   }
+
+   useEffect(() => {
+      getTodos()
+   }, []);
 
    return (
        <>
-
+          <div>{todos.map(todo => <div key={todo.id}>{todo.title}</div>)}</div>
        </>
    );
 }
